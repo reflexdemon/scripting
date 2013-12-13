ff09cf40c0ed047bf8cd8ccb07061649e41f120d
reflexdemon <venkateswara.venkatraman@cbeyond.net>
2013-12-13 14:36:24 -0500

    Minor Changes

--- test_url.sh
+++ test_url.sh
@@ -15,5 +15,9 @@ do
 
 	rm -f $FILE_NAME
 	echo Size is $s1 bytes
-	echo Size is $s2
+	if [ "$s1" -le 0 ]; then
+		echo "Test Failed."
+	else
+		echo "Test Passed " + $s2 + " data downloaded."
+	fi
 done

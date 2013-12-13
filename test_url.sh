ef2753c1e07c6c021503431c7b55a19b2deb8c18
reflexdemon <venkateswara.venkatraman@cbeyond.net>
2013-12-13 14:57:02 -0500

    Silent Mode :)

--- test_url.sh
+++ test_url.sh
@@ -7,7 +7,7 @@ FILE_NAME=test.wsdl
 
 for ver in ${URL}
 do
-	curl $URL >$FILE_NAME >nul
+	curl $URL >$FILE_NAME
 	#wc -c <  $FILE_NAME
 	#ls -lah $FILE_NAME | awk '{ print $5}'
 	s1=$(wc -c < $FILE_NAME)

186ac537147d86726af58d4167a33a6fe34b2eb7
reflexdemon <venkateswara.venkatraman@cbeyond.net>
2013-12-13 14:32:06 -0500

    Minor Changes

--- test_url.sh
+++ test_url.sh
@@ -10,8 +10,10 @@ do
 	curl $URL >$FILE_NAME
 	#wc -c <  $FILE_NAME
 	#ls -lah $FILE_NAME | awk '{ print $5}'
-	s=$(wc -c < $FILE_NAME)
+	s1=$(wc -c < $FILE_NAME)
+	s2=$(ls -lah $FILE_NAME | awk '{ print $5}')
 
 	rm -f $FILE_NAME
-	echo Size is $s
+	echo Size is $s1 bytes
+	echo Size is $s2
 done

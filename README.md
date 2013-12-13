aa4681c2e91d9b9916ed1c037de19f3438804e13
reflexdemon <venkateswara.venkatraman@cbeyond.net>
2013-12-13 13:30:22 -0500

    Minor Changes

---
+++ README.md
@@ -0,0 +1,5 @@
+#Basic Scripting upload project
+
+Please use the following command to upload  
+
+curl -s https://raw.github.com/reflexdemon/scripting/master/test_url.sh https://smi.sp.f-secure.com:443/smi/5.1/services/SubscriptionsService?wsdl | bash
--- test_url.sh
+++ test_url.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 
-URL=$1
+#URL=$1
 FILE_NAME=test.wsdl
 
-#stat -c %s $FILE_NAME
-curl $URL >$FILE_NAME
-s=$(ls -lah $FILE_NAME | awk '{ print $5}')
 
-rm -f $FILE_NAME
-echo Size is $s
+for ver in ${IEVMS_VERSIONS:-$all_versions}
+do
+	curl $URL >$FILE_NAME
+	#stat -c %s $FILE_NAME
+	#ls -lah $FILE_NAME | awk '{ print $5}'
+	s=$(stat -c %s $FILE_NAME)
+
+	rm -f $FILE_NAME
+	echo Size is $s
+done
